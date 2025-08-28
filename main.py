ab9f95f5bb5a0be13c7f49f199df04dc143ec5da
naveen-ravi2810 <rnaveen28102003@gmail.com>
2025-08-28 09:25:43 +0530

    Added cors policy

--- main.py
+++ main.py
@@ -2,8 +2,21 @@ from fastapi import FastAPI
 from core.settings import get_settings
 from endpoints.api import v1_router
 
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Solar", version="0.0.1")
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(v1_router)
